035cc1f1bd86f68b252ecff9fca60a3d922a0076
Mari Sosa <msosa32@gmail.com>
2018-03-29 15:45:51 -0700

    updated p_anneal_segments.py

--- python/anneal_onlyneighbor_segments/p_anneal_segments.py
+++ python/anneal_onlyneighbor_segments/p_anneal_segments.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-import sys, os
+import sys, os, pdb
+
 
 parent_path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('/opt/mountainlab/packages/pyms')
@@ -9,11 +10,9 @@ sys.path.append('/opt/mountainlab/packages/pyms')
 from basic.p_extract_clips import extract_clips_helper
 
 from mlpy import readmda, writemda64, writemda32, DiskReadMda
-import itertools as it
-from scipy.special import comb
 
 processor_name='pyms.anneal_segments'
-processor_version='0.17'
+processor_version='0.11'
 
 def anneal_segments(*, timeseries_list, firings_list, firings_out, dmatrix_out='', k1_dmatrix_out='', k2_dmatrix_out='', dmatrix_templates_out='', time_offsets):
     """
@@ -49,6 +48,7 @@ def anneal_segments(*, timeseries_list, firings_list, firings_out, dmatrix_out='
     print('time_offsets ' + str(time_offsets))
     if time_offsets:
         time_offsets=np.fromstring(time_offsets,dtype=np.float_,sep=',')
+        #print('time_offsets ' + str(time_offsets))
     else:
         print('No time offsets provided - assuming zero time gap/continuously recorded data')
         time_offsets=np.zeros(len(timeseries_list))
@@ -60,14 +60,14 @@ def anneal_segments(*, timeseries_list, firings_list, firings_out, dmatrix_out='
 
     concatenated_firings = concat_and_increment(firings_list, time_offsets)
 
-    (dmatrix, k1_dmatrix, k2_dmatrix, templates, Kmaxes, segment_combos) = get_dmatrix_templates(timeseries_list, firings_list)
+    (dmatrix, k1_dmatrix, k2_dmatrix, templates, Kmaxes) = get_dmatrix_templates(timeseries_list, firings_list)
     dmatrix[np.isnan(dmatrix)] = -1; # set nans to -1 to avoid runtime error
     k1_dmatrix[dmatrix < 0] = np.nan  # replace all negative dist numbers (no comparison) with NaN
     k2_dmatrix[dmatrix < 0] = np.nan # replace all negative dist numbers (no comparison) with NaN
     dmatrix[dmatrix < 0] = np.nan  # then replace all negative dist numbers (no comparison) with NaN
 
     #TODO: Improve join function
-    pairs_to_merge = get_join_matrix(dmatrix, k1_dmatrix, templates, Kmaxes, segment_combos) # Returns with base 1 adjustment
+    pairs_to_merge = get_join_matrix(dmatrix, k1_dmatrix, templates, Kmaxes) # Returns with base 1 adjustment
 
     pairs_to_merge = np.reshape(pairs_to_merge, (-1, 2))
     pairs_to_merge = pairs_to_merge[~np.isnan(pairs_to_merge).any(axis=1)] # Eliminate all rows with NaN
@@ -89,16 +89,12 @@ def anneal_segments(*, timeseries_list, firings_list, firings_out, dmatrix_out='
     #Write
     return writemda64(concatenated_firings, firings_out)
 
-def get_join_matrix(dmatrix, k1_dmatrix, templates, Kmaxes, segment_combos):
+def get_join_matrix(dmatrix, k1_dmatrix, templates, Kmaxes):
     #Sweep forward in time, linking clust to min dist away
     pairs_to_merge=np.array([])
-    adj_Kmaxes = np.cumsum(Kmaxes)
+    f1_adj = 0
+    f2_adj = Kmaxes[0]
     for dframe in range(dmatrix.shape[2]):
-        if segment_combos[dframe,0]==0: #if starting with the first segment and combination, 0 offset
-            f1_adj=0
-        else:
-            f1_adj=adj_Kmaxes[segment_combos[dframe,0]-1] #otherwise increment cluster numbers based on max of previous segment
-        f2_adj=adj_Kmaxes[segment_combos[dframe,1]-1]
         for f1_idx in range(dmatrix.shape[1]):
             f2_pair = _nanargmin(dmatrix[f1_idx,:,dframe]) #Ignore nan's and if all nans, return nan
             if not np.isnan(f2_pair):
@@ -106,9 +102,9 @@ def get_join_matrix(dmatrix, k1_dmatrix, templates, Kmaxes, segment_combos):
                 if f1_pair==f1_idx: # mutual nearest
                     #Check if distance to new template is less than mean distance to spike
                     if ((dmatrix[f1_idx, f2_pair, dframe]/k1_dmatrix[f1_idx, f2_pair, dframe]) < 1): #changed from 0.1
-                        pairs_to_merge = np.append(pairs_to_merge, np.array([f1_idx + f1_adj + 1, f2_pair + f2_adj + 1])) #Base 1 adjustment to match label
-        #f1_adj+=Kmaxes[segment_combos[dframe,0]]
-        #f2_adj+=Kmaxes[segment_combos[dframe,1]]
+                        pairs_to_merge = np.append(pairs_to_merge, np.array([f1_idx + f1_adj + 1, f2_pair + f2_adj + 1])) #Base 1 adjustment to match label                 
+        f1_adj+=Kmaxes[dframe]
+        f2_adj+=Kmaxes[dframe+1]
     return pairs_to_merge
 
 def _nanargmin(X):
@@ -145,11 +141,6 @@ def get_dmatrix_templates(timeseries_list, firings_list):
     M = X.N1()
     clip_size = 50
     num_segments = len(timeseries_list)
-    segment_combos = it.combinations(range(num_segments),2) # Get all possible segment combinations
-    segment_combos = np.array(list(segment_combos))
-    # Order segment combinations such that neighbors are first, then non-neighbors
-    segment_combos = np.append(segment_combos[np.where(np.diff(segment_combos)==1)[0],:],segment_combos[np.where(np.diff(segment_combos)>1)[0],:],axis=0)
-    num_combos = int(comb(num_segments,2))
     firings_arrays = []
     Kmaxes=[]
     for j in range(num_segments):
@@ -168,28 +159,27 @@ def get_dmatrix_templates(timeseries_list, firings_list):
             Kmaxes.append(np.max(labels))
     if max(Kmaxes)>0:
         use_max = int(max(Kmaxes))
-        dmatrix = np.ones((use_max, use_max, num_combos)) * (-1)
-        k1_dmatrix = np.ones((use_max, use_max, num_combos)) * (-1)
-        k2_dmatrix = np.ones((use_max, use_max, num_combos)) * (-1)
-        templates = np.zeros((M, clip_size, use_max, 2 * num_combos))
-    
-    for n in range(num_combos):
-        # count up to number of combinations for dmatrix 3rd dimension indexing
-        j1 = segment_combos[n,0];
-        j2 = segment_combos[n,1];
-        print('Computing dmatrix between segments %d and %d' % (j1, j2))
+        dmatrix = np.ones((use_max, use_max, num_segments - 1)) * (-1)
+        k1_dmatrix = np.ones((use_max, use_max, num_segments - 1)) * (-1)
+        k2_dmatrix = np.ones((use_max, use_max, num_segments - 1)) * (-1)
+        templates = np.zeros((M, clip_size, use_max, 2 * (num_segments - 1)))
+
+    for j in range(num_segments - 1):
+        print('Computing dmatrix between segments %d and %d' % (j, j + 1))
         #print(timeseries_list)
-        if np.size(firings_arrays[j1])==0 or np.size(firings_arrays[j2])==0:
+        if np.size(firings_arrays[j])==0 or np.size(firings_arrays[j+1])==0:
             #templates = np.zeros((M, clip_size, 1))
             continue
         else:
-            (dmatrix0, k1_dmatrix0, k2_dmatrix0, templates1, templates2) = compute_dmatrix(timeseries_list[j1], timeseries_list[j2],firings_arrays[j1], firings_arrays[j2],clip_size=clip_size)
-            dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], n] = dmatrix0
-            k1_dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], n] = k1_dmatrix0
-            k2_dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], n] = k2_dmatrix0
-            templates[:, :, 0:dmatrix0.shape[0], n * 2] = templates1
-            templates[:, :, 0:dmatrix0.shape[1], n * 2 + 1] = templates2
-    return (dmatrix,  k1_dmatrix, k2_dmatrix, templates, Kmaxes, segment_combos)
+            (dmatrix0, k1_dmatrix0, k2_dmatrix0, templates1, templates2) = compute_dmatrix(timeseries_list[j], timeseries_list[j + 1],
+                firings_arrays[j], firings_arrays[j + 1],
+                clip_size=clip_size)
+            dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], j] = dmatrix0
+            k1_dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], j] = k1_dmatrix0
+            k2_dmatrix[0:dmatrix0.shape[0], 0:dmatrix0.shape[1], j] = k2_dmatrix0
+            templates[:, :, 0:dmatrix0.shape[0], j * 2] = templates1
+            templates[:, :, 0:dmatrix0.shape[1], j * 2 + 1] = templates2
+    return (dmatrix, k1_dmatrix, k2_dmatrix, templates, Kmaxes)
 
 def compute_dmatrix(timeseries1, timeseries2, F1, F2, *, clip_size):
     X = DiskReadMda(timeseries1)
@@ -206,7 +196,7 @@ def compute_dmatrix(timeseries1, timeseries2, F1, F2, *, clip_size):
     K1 = int(max(labels1)) 
     K2 = int(max(labels2))
     dmatrix = np.zeros((K1, K2))
-    
+        
     k1_dmatrix = np.zeros((K1, K2))
     k2_dmatrix = np.zeros((K1, K2))
 
@@ -239,7 +229,7 @@ def get_first_events(firings, num):
         times_k = times[inds_k]
         if (len(times_k) <= num):
             to_use[inds_k] = 1 #if whole cluster < num spikes, use all the spikes
-        else: # otherwise use the first and last num spikes
+        else: # otherwise use the first and last 100 spikes
             times_k_sorted = np.sort(times_k)
             cutoff = times_k_sorted[num]
             to_use[inds_k[np.where(times_k <= cutoff)[0]]] = 1
@@ -275,7 +265,6 @@ def compute_distance_between_template_and_spikes(clips):
     for idx in range(clips.shape[2]):
         spk_mean_dist += np.sum(((centroid1 - clips[:,:,idx]) ** 2))/clips.shape[2]
     return spk_mean_dist
-
 
 anneal_segments.name = processor_name
 anneal_segments.version = processor_version
